fa4321fa5370cc9f93eef619cee27a45c944ed52
iamisna <iamisna@naver.com>
2021-01-24 19:56:56 +0900

    추가.

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -10,6 +10,8 @@ public class Main {
 	// write your code here
         System.out.println("interface==");
         interfaceNotIf();
+        System.out.println("tryInterface==");
+        tryInterfaceNotIf();
         System.out.println("lambda==");
         lambdaNotIf();
         System.out.println("try==");
@@ -25,6 +27,7 @@ public class Main {
 
         String data1 = "TEST";
         String data2 = "TEST1";
+        String data3 = "TEST2";
 
         // reuqest 데이터가 null 일경우
         typeMap.put(null, new SampleA());
@@ -33,9 +36,41 @@ public class Main {
         // request 데이터가 일치 할경우
         typeMap.put(compare, new SampleC());
         // request 데이터가 불일치 할경우.
-        typeMap.put("notCompare", new SampleB());
+        typeMap.put("notCompare", new SampleD());
 
-        String str = typeMap.get(request)==null?typeMap.get("notCompare").validate(data1, data2):typeMap.get(request).validate(data1, data2);
+        String str = typeMap.get(request)==null?
+                     typeMap.get("notCompare").validate(data1, data2, data3):
+                     typeMap.get(request).validate(data1, data2, data3);
+
+        System.out.println(str);
+    }
+
+    //interface 를 이용한 클래스 분류.
+    public static void tryInterfaceNotIf() {
+        Map<String, SampleInterface> typeMap = new HashMap<>();
+
+        String request = "B";
+        String compare = "A";
+
+        String data1 = "TEST";
+        String data2 = "TEST1";
+        String data3 = "TEST2";
+
+        // reuqest 데이터가 null 일경우
+        typeMap.put(null, new SampleA());
+        // request 데이터가 빈값 일 경우
+        typeMap.put("", new SampleB());
+        // request 데이터가 일치 할경우
+        typeMap.put(compare, new SampleC());
+        // request 데이터가 불일치 할경우.
+        typeMap.put("notCompare", new SampleD());
+
+        String str = "";
+        try {
+            str = typeMap.get(request).validate(data1, data2, data3);
+        } catch (NullPointerException npe) {
+            str = typeMap.get("notCompare").validate(data1, data2, data3);
+        }
 
         System.out.println(str);
     }
--- src/com/test/SampleInterface.java
+++ src/com/test/SampleInterface.java
@@ -1,14 +1,14 @@
 package com.test;
 
 public interface SampleInterface {
-    public String validate(String data1, String data2);
+    public String validate(String data1, String data2, String data3);
 }
 
 class SampleA implements SampleInterface {
 
     @Override
-    public String validate(String data1, String data2) {
-        return "A :" + data1 + " " + data2;
+    public String validate(String data1, String data2, String data3) {
+        return "데이터가 NULL 일경우  :" + data1 + " " + data2;
 
     }
 }
@@ -16,15 +16,23 @@ class SampleA implements SampleInterface {
 class SampleB implements SampleInterface {
 
     @Override
-    public String validate(String data1, String data2) {
-        return "B :" + data1 + " " + data2;
+    public String validate(String data1, String data2, String data3) {
+        return "데이터가 빈겺일 경우 :" + data1 + " " + data2;
     }
 }
 
 class SampleC implements SampleInterface {
 
     @Override
-    public String validate(String data1, String data2) {
-        return "C :"+ data1 +" " + data2;
+    public String validate(String data1, String data2, String data3) {
+        return "데이터가 일치 할경우 :"+ data1 +" " + data2;
+    }
+}
+
+class SampleD implements SampleInterface {
+
+    @Override
+    public String validate(String data1, String data2, String data3) {
+        return "데이터가 불일치 할경우 :"+ data1 +" " + data2;
     }
 }
